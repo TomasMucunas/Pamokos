72935223b6fec5234346dd2f9bac4baa0f7b481d
TomasMucunas <tomas.muczunas@gmail.com>
2025-02-18 13:37:33 +0300

    Update server.js

--- tinklu-kurimas/37 - diena(KD)/KD Express/server.js
+++ tinklu-kurimas/37 - diena(KD)/KD Express/server.js
@@ -1,4 +1,3 @@
-// Сервер.js
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
@@ -15,20 +14,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// Маршруты
-
 app.use('/auth', userRoutes);
 app.use('/authors', authorRoutes);
 app.use('/books', bookRoutes);
 
-// Сделки с ошибками
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
 });
 
-// Начальный сервер
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
